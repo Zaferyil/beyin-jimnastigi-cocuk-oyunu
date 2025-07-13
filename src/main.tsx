0f95bce9b31aee67ff098ab8b3755730d32623b4
Zaferyil <163585736+Zaferyil@users.noreply.github.com>
2025-07-13 19:24:24 +0200

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CognitiveGameForKids from './cognitive_game_kids.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <CognitiveGameForKids />
+  </React.StrictMode>,
+)
